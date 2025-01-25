7b6ac6ccd2ab86c1ad2d610bdcd61414bc4404d3
igorcamposdeborba <igor_campos5@hotmail.com>
2025-01-25 12:12:22 -0300

    Added JPA database config

--- src/main/kotlin/com/igorborba/crud/domain/entities/Customer.kt
+++ src/main/kotlin/com/igorborba/crud/domain/entities/Customer.kt
@@ -1,10 +1,20 @@
 package com.igorborba.crud.domain.entities
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import org.springframework.data.annotation.Id
 import java.util.UUID
 
+
+@Entity(name = "customer")
 class Customer(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     val id: UUID,
     var name: String,
-    var email: String
+    var email: String,
+    var cpf: String
 ) {
 }
